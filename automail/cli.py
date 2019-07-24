e59abe37d872ea137bf17a2d1bef6ded4aacf444
ronald <ronald-farias@outlook.com>
2019-07-23 22:17:47 -0300

    fixed flake8 issue

--- automail/cli.py
+++ automail/cli.py
@@ -58,8 +58,7 @@ def main():
                 with open(".coveragerc", "a+") as _f:
                     covignore = f"[run]\nomit = {env_dir}/*"
                     _f.write(covignore)
-            command_line = FIELDS["test_command"].split(" ") + ["--cov"] 
-
+            command_line = FIELDS["test_command"].split(" ") + ["--cov"]
         else:
             command_line = FIELDS["test_command"].split(" ")
 
@@ -73,7 +72,9 @@ def main():
             cov_output = command.stdout.decode()
             tests_passing = not bool(command.returncode)
         except BaseException:
-            print(Fore.RED + "An error occurred when running test command, see nina.log")
+            print(
+                Fore.RED + "An error occurred when running test command, "
+                "see nina.log")
             cov_output = ""
             tests_passing = False
 
